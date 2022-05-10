5dfbb72280f6c1b27685cc5f75bb53a19375d1e3
xxuxa-k <tk0x2bc@gmail.com>
2022-05-10 12:31:12 +0900

    lint設定を追加

--- src/@types/addon.d.ts
+++ src/@types/addon.d.ts
@@ -1,4 +1,4 @@
-declare module GoogleAppsScript {
+declare namespace GoogleAppsScript {
   // https://developers.google.com/apps-script/manifest/gmail-addons
   namespace Addons {
     interface GmailEventObject {
@@ -20,14 +20,15 @@ declare module GoogleAppsScript {
 }
 
 declare const dayjs: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   dayjs(arg?: any): any;
 }
 
 declare const global: {
-  onGmailCompose(arg?: any): GoogleAppsScript.Card_Service.Card;
-  handleFormInput(arg?: any): void;
-  handleSendButtonClick(arg?: any):GoogleAppsScript.Card_Service.Card | undefined;
-  saveAttachments(arg?: any?): void;
+  onGmailCompose(e: GoogleAppsScript.Addons.GmailEventObject): GoogleAppsScript.Card_Service.Card;
+  handleFormInput(e: { parameters: GoogleAppsScript.Addons.OnChangeActionsParameters}): void;
+  handleSendButtonClick(e: { parameters: GoogleAppsScript.Addons.OnChangeActionsParameters}): GoogleAppsScript.Card_Service.Card | undefined;
+  saveAttachments(e: { parameters: GoogleAppsScript.Addons.OnChangeActionsParameters}): void;
   logger(text: string): void;
   governments: string[];
 }
